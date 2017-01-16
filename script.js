245aa33282d92678bd26b82471c999319108e1fc
Arrielle <arrielle@Arrielles-MBP.hsd1.mn.comcast.net>
2017-01-15 21:19:48 -0600

    comments

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ return z - 4;
 }
 
 var final = add(multiply(subtract(myNum)));
+
+/*
+My inital code was much messier but I got some tips
+from a friend that knows javascript. He said that with
+my code the way it is, I could run into 'variable scope'
+issues. I think this is much cleaner and makes my code
+much more flexible.
+
+Here is my initial code so you can see my thought process;
+
+var myNum = 6;
+
+function add(myNum) {
+  myNum = myNum + 2;
+  return myNum;
+}
+function multiply(myNum) {
+  myNum = myNum * 3;
+  return myNum;
+}
+function subtract(myNum) {
+  myNum = myNum - 4;
+  return myNum;
+}
+
+var final = add(multiply(subtract(myNum)));
+*/
