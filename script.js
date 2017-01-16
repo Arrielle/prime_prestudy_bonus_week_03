c1e3cb4a05239593dc4d314eda4d58b740319843
Arrielle <arrielle@Arrielles-MBP.hsd1.mn.comcast.net>
2017-01-15 21:12:57 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var myNum = 6;
+
+function add(x) {
+return x + 2;
+}
+
+function multiply(y) {
+return y * 3;
+}
+
+function subtract(z) {
+return z - 4;
+}
+
+var final = add(multiply(subtract(myNum)));
